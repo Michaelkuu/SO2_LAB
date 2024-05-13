4933ad098e76fbc7beeae4e6a474966c95712af4
Michaelkuu <85109674+Michaelkuu@users.noreply.github.com>
2024-05-13 23:56:10 +0200

    zadanka.sh

--- lab3/zadanka.sh
+++ lab3/zadanka.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #fakaping.sh:
-./fakaping.sh > /dev/null | sort
+./fakaping.sh > /dev/null 2>&1 | sort
 ./fakaping.sh 2>&1 | sort -u | tee all.log
 
 #access_log:
 grep -oE '^([0-9]{1,3}\.){3}[0-9]{1,3}' access_log.txt | sort | uniq | head -n 10
-
+grep '"DELETE' access_log.txt | awk '{print $7}' | sort | uniq
 
 #groovies:
 
 #yolo.csv:
 grep -E '^([^,][13579],)' yolo.csv 1>&2
-grep -oE '\b[0-9].[0-9].[0-9].[0-9]*\b' yolo.csv
+grep -E '\$2\.99|\$5\.99|\$9\.99' yolo.csv | awk -F, '{print $3, $7}' 1>&2
 
